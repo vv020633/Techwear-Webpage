534ce6eb9c6b91c3c8a7714b0875932b10f2aa11
Chas <chas.mclaughlin1@gmail.com>
2018-12-28 01:05:12 -0500

    Gallery added as a static webpage reachable from the mainpage. Modifications to the layout of the gallery in progress.

--- expressnode.js
+++ expressnode.js
@@ -1,28 +1,17 @@
 const express = require('express');
 const app = express();
-const path = require("path");
-const bodyParser = require('body-parser');
-var urlencodedParser = bodyParser.urlencoded({ extended: true});
-//require express packages
+//const path = require("path");
 app.use(express.static('public'));
-//app.use(bodyParser.json());
-
-//Middleware
+//app.use(express.static('views'));
+//app.use(express.static(__dirname + '/public'));
 app.set('view engine', 'ejs');
-//set up template engine
-//app.get('/', function (req,res) {
- //res.sendFile(__dirname +'public/index.html');
-//})
-app.get('/', function (req,res) {
- res.render('index');
- //res.render used instead of res.send when working with a templating language (ejs)
+
+app.get('/', function (req, res) {
+ res.render('index')
 })
-app.post('/', urlencodedParser, function(req, res){
-  console.log(req.body);
-  res.render('index',{qs: req.query});
-});
 
+//app.use('/home', express.static('Techwear-Webpage'));
 
-app.listen(8080, function () {
+app.listen(8080, function() {
   console.log("Server running on port 8080");
 });
